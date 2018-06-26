d48ca7742e61fa4829d874cd57959c99bfa0ca7f
LoRosset <loicrosset1@gmail.com>
2018-06-26 20:47:25 +0200

    mongoose models

---
+++ backend/models/user.js
@@ -0,0 +1,20 @@
+ const mongoose = require(‘mongoose’);
+ require('mongoose-type-email');
+ 
+ // Declare Schema
+ const UserSchema = new mongoose.Schema(
+ 	{
+	 	name: { type: String, required: true},
+	 	home: { type: Schema.ObjectId, ref: 'Home'},
+	 	username: {type: String, required: true, min: 3, max:20},
+	 	password: {type: String, required: true, min: 6, max: 40},
+	 	email: {type: mongoose.SchemaTypes.Email, required: true}
+ 	}, 
+ 	{ timestamps: true }
+ );
+ 
+ // Declare Model to mongoose with Schema
+ const User = mongoose.model('User' UserSchema);
+ 
+ // Export Model to be used in Node
+ module.exports = mongoose.model('User');
