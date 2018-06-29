b49e167ab95c91142f1e67d07deca7cd6fb35bb6
Loic <loicrosset1@gmail.com>
2018-06-29 12:34:31 +0200

    Routes and controllers

---
+++ backend/controllers/home.js
@@ -0,0 +1,41 @@
+const Home = require('../models/home');
+
+	async function find (ctx) {
+		const homes = await Todo.find({})
+		ctx.body = homes
+	}
+
+	async function create (ctx) {
+		const newHouse = new House(ctx.request.body)
+		const savedHouse = await newHouse.save()
+		ctx.body = savedHouse
+	}
+
+	async function update (ctx) {
+		//Find Home based on id
+		const id = ctx.params.id
+		const home = await Home.findById(id)
+
+		//Update home in database
+		const updatedHome = await home.save()
+		ctx.body = updatedHome
+	}
+
+	async function delete (ctx) {
+		//Find Home based on id
+		const id = ctx.params.id
+		const home = await Home.findById(id)
+
+		//Delete home from database
+		const deletedHome = await home.remove()
+		ctx.body = deletedHome
+	}
+
+
+
+module.exports = {
+	find,
+	create,
+	update,
+	delete
+}
---
+++ backend/routes/app.js
@@ -0,0 +1,75 @@
+const Router = require('koa-router')
+const router = new Router()
+
+//Controller modules
+const home_controller = require('../controllers/home')
+const user_controller = require('../controllers/user')
+const camera_controller = require('../controllers/camera')
+const box_controller = require('../controllers/box')
+
+
+//Routes path
+
+			//-----USER ROUTES-----//
+//GET login page
+router.get('/', function(req,res){
+	res.redirect('/login')
+})
+
+//GET list of all users
+router.get('/user', user_controller.find)
+
+//CREATE a user
+router.post('/user/create', user_controller.create)
+
+//UPDATE a user
+router.post('/user/:id/update', user_controller.update)
+
+//DELETE a user
+router.delete('/user/:id', user_controller.delete)
+
+//POST login information for authentification
+router.post('/login', user_controller.control)
+
+
+			//-----HOME ROUTES-----//
+//GET account page with homes of user
+router.get('/user/:id', home_controller.find)
+
+//CREATE a home for a specific user
+router.post('/user/:id/home/create', home_controller.create)
+
+//UPDATE a specific home
+router.post('/user/:id/home/:id/update', home_controller.update)
+
+//DELETE a specific home
+router.delete('/user/:id/home/:id', home_controller.delete)
+
+
+			//-----BOX ROUTES-----//
+//GET boxes for a specific home
+router.get('/user/:id/home/:id/', box_controller.find)
+
+//CREATE a box for a specific home
+router.post('/user/:id/home/:id/box/create', box_controller.create)
+
+//UPDATE a specific box
+router.post('/user/:id/home/:id/box/:id', box_controller.update)
+
+//DELETE a specific box
+router.delete('/user/:id/home/:id/box/:id', box_controller.delete)
+			
+			//-----CAMERA ROUTES-----//
+//GET cameras for a specific box
+router.get('/user/:id/home/:id/box/:id/', camera_controller.find)
+
+//CREATE a camera for a specific box
+router.post('/user/:id/home/:id/box/:id/camera/create', camera_controller.create)
+
+//UPDATE a specific camera
+router.post('/user/:id/home/:id/box/:id/camera/:id', camera_controller.update)
+
+//DELETE a specific camera
+router.delete('/user/:id/home/:id/box/:id/camera/:id', camera_controller.delete)
+
+module.exports = router.routes()
