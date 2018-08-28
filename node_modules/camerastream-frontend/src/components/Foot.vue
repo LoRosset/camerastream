83888c39cf411b05b7aef144ffc1c38c495011fb
Loic <loicrosset1@gmail.com>
2018-08-28 16:01:38 +0200

    some changes

---
+++ node_modules/camerastream-frontend/src/components/Foot.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-footer color="green" app>
+      <span class="white--text">&copy; Loïc Rosset, 2018</span>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Foot'
+}
+</script>
